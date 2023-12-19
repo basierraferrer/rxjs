533586be62778eb3660ce21c4c0981ac5923d56b
Brian Sierra <brian.sierra@globant.com>
2023-12-19 14:08:18 -0500

    Class 7: Operators - distinc, distinctUntilChange &  distinctUntilKeyChange

---
+++ src/classes/operators/distincts.js
@@ -0,0 +1,30 @@
+import {
+  of,
+  distinct,
+  distinctUntilChanged,
+  distinctUntilKeyChanged,
+} from "rxjs";
+
+const repeatedNumber$ = of(
+  { k: 1 },
+  { k: 2 },
+  { k: 3 },
+  { k: 3 },
+  { k: 4 },
+  { k: 3 },
+  { k: 2 },
+  { k: 1 },
+  { k: 2 },
+  { k: 2 },
+  { k: 2 },
+  { k: 3 }
+).pipe(
+  //just emit differents values
+  //distinct(),
+  //just emit if the current value is different the previus one
+  //distinctUntilChanged()
+  //
+  distinctUntilKeyChanged("k")
+);
+
+repeatedNumber$.subscribe(console.log);
