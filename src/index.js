29104bef945fcd266e26023c175f7cd54f90eafb
Brian Sierra <brian.sierra@globant.com>
2023-12-19 14:19:13 -0500

    Added Classes 7 & 8

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-// Class 1
-import "./classes/observable/observable_observer";
-// Class 2
-import "./classes/subject/subject";
-// Class 3
-import "./classes/fromEvent";
-// Class 4
-import "./classes/operators/from_of";
-// Class 5
-import "./classes/operators/interval_timer";
-// Class 6
-import "./classes/operators/map_filter_reduce";
+// // Class 1
+// import "./classes/observable/observable_observer";
+// // Class 2
+// import "./classes/subject/subject";
+// // Class 3
+// import "./classes/fromEvent";
+// // Class 4
+// import "./classes/operators/from_of";
+// // Class 5
+// import "./classes/operators/interval_timer";
+// // Class 6
+// import "./classes/operators/map_filter_reduce";
+// // Class 7
+// import "./classes/operators/distincts";
+// Class 8
+import "./classes/operators/times";
